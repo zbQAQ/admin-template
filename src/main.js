075fd09d7e99193000df2a049a495391fb94b165
zhouruibin <1290368140@qq.com>
2019-12-30 18:43:43 +0800

    完成layout 路由布局

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./permission.js";
+
+import Element from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+import "@/styles/index.css"; // global css
+
+Vue.use(Element, { size: "mini" });
 
 Vue.config.productionTip = false;
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,32 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+
+import Layout from "../layout/index.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: Layout,
+    redirect: "/overview",
+    children: [
+      {
+        path: "overview",
+        name: "overview",
+        component: () => import("../views/overview.vue")
+      },
+      {
+        path: "about",
+        name: "about",
+        component: () => import("../views/About.vue")
+      }
+    ]
   }
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  // mode: "history", // require service support
   base: process.env.BASE_URL,
   routes
 });
